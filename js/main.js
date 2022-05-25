992de561f83fce62d59c1c94b7708ca096d72542
rauldangio <jojofagqwrt@gmail.com>
2022-05-25 16:06:30 -0300

    comandos basicos do js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Raul";
+var idade = "16";
+var idade2 = "11";
+var frase = "Russia é o melhor time do mundo";
+//alert("Olá, Mundo! O "+nome+" tem "+idade+" anos");
+//alert(idade + idade2);
+alert(frase.toLowerCase());
+alert(frase.toUpperCase());
+alert(frase.replace("Russia","Brasil").toUpperCase());
