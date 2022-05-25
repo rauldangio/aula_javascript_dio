e3701e917a6f0b362e2a6c5749d9be5b3e982495
rauldangio <jojofagqwrt@gmail.com>
2022-05-25 18:47:13 -0300

    usando js em elementos html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/*
 var abc = 0;
 function addOne(n1){
     
@@ -12,6 +12,30 @@ function addOne(n1){
 var n1 = prompt("Digite um numero")
 addOne(n1)
 console.log(abc)
+*/
 
+function clique(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>"
+}
 
+function redirecionamento(){
+    //window.open("https://www.google.com")
+    window.location.href = "https://www.google.com"
+}
+
+function mouse(elemento){
+    elemento.innerHTML = "Obrigado por passar"
+}
+
+function colocamouse(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("A Gi é fedorenta")
+}
+
+function fchange(elemento){
+    console.log(elemento.value)
+}
 
