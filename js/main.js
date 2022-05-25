597e5e3a2f77b7cd0ec2487410102069a0071811
rauldangio <jojofagqwrt@gmail.com>
2022-05-25 17:39:13 -0300

    functions

--- js/main.js
+++ js/main.js
@@ -1,49 +1,17 @@
-/*var lista = ["maça","laranja","melancia"];
-lista.push("uva");
-lista.pop();*/
 
-//console.log(lista.length)
-//console.log(lista.reverse())
-/*console.log(lista);
-console.log(lista.toString());
-console.log(lista[0]);
-console.log(lista.toString()[0]);
-console.log(lista.join(" - "));
-*/
+var abc = 0;
+function addOne(n1){
+    
+    if (n1 > 6){
+        return abc = true;
+    }else {
+        return abc = false;
+    }
+}
 
-/*var frutas = [{nome: "pera", cor: "verde"},{nome: "melancia", cor:"vermelho"}];
-console.log(frutas[1].nome)
-console.log(frutas[0].cor)*/
-
-/*
-var champ = prompt("Qual campeão do lol vc joga?");
-if (champ == "shaco"){
-    alert("doente");
-}else{
-    alert("Boa escolha!");
-};
-*/
-
-/*
-var contador = 0;
-while(contador <=5){
-    console.log(contador);
-    contador++;
-};
-*/
-
-/*
-for(contador = 0; contador <= 10; contador++){
-    console.log(contador);
-};
-*/
-
-var date = new Date();
-alert(date.getDate());
-alert(date.getFullYear());
-
-
-
+var n1 = prompt("Digite um numero")
+addOne(n1)
+console.log(abc)
 
 
 
