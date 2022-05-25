80775b9ef0a7487686438b2215c6c4f253ce8b0a
rauldangio <jojofagqwrt@gmail.com>
2022-05-25 16:56:19 -0300

    condicionais, laços de repetição e date

--- js/main.js
+++ js/main.js
@@ -11,6 +11,39 @@ console.log(lista.toString()[0]);
 console.log(lista.join(" - "));
 */
 
-var frutas = [{nome: "pera", cor: "verde"},{nome: "melancia", cor:"vermelho"}];
+/*var frutas = [{nome: "pera", cor: "verde"},{nome: "melancia", cor:"vermelho"}];
 console.log(frutas[1].nome)
-console.log(frutas[0].cor)
+console.log(frutas[0].cor)*/
+
+/*
+var champ = prompt("Qual campeão do lol vc joga?");
+if (champ == "shaco"){
+    alert("doente");
+}else{
+    alert("Boa escolha!");
+};
+*/
+
+/*
+var contador = 0;
+while(contador <=5){
+    console.log(contador);
+    contador++;
+};
+*/
+
+/*
+for(contador = 0; contador <= 10; contador++){
+    console.log(contador);
+};
+*/
+
+var date = new Date();
+alert(date.getDate());
+alert(date.getFullYear());
+
+
+
+
+
+
